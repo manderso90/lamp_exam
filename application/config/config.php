e99003de970fa7406bc785302f30f992e4221d73
Manderson <Manderson>
2016-07-20 18:02:43 -0700

    Work

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ec2-54-200-69-108.us-west-2.compute.amazonaws.com/';
+$config['base_url'] = 'http://ec2-54-200-69-108.us-west-2.compute.amazonaws.com';
 
 /*
 |--------------------------------------------------------------------------
